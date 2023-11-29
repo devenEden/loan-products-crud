738908af0665d3be41da0d0e81b3d89df1528167
Feta Deven <devenfeta19@gmail.com>
2023-11-29 15:21:04 +0300

    sgi fix

--- core/wsgi.py
+++ core/wsgi.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 # assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
-path = '/home/myusername/mysite'
+path = '/home/fetaDeven/loan-app'
 if path not in sys.path:
     sys.path.insert(0, path)
 
